547ace60a7988f3900976120d219fb991ab3177c
prasad-vamer <prasadkrishnakptp@gmail.com>
2025-02-26 01:06:39 +0900

    feat: add DevInstanceEc2Stack for provisioning EC2 instances with configurable properties

---
+++ app/lib/services/dev-instance-provider/stack/dev-instance-ec2-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Environment } from "aws-cdk-lib";
+import { Ec2Construct, Ec2ConstructProps } from "../constructs/Ec2Construct";
+
+interface DevInstanceEc2StackProps extends cdk.StackProps, Ec2ConstructProps {
+  env?: Environment;
+}
+
+export class DevInstanceEc2Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: DevInstanceEc2StackProps) {
+    super(scope, id, props);
+
+    new Ec2Construct(this, "Ec2Construct", {
+      vpc: props.vpc,
+      securityGroup: props.securityGroup,
+      keyPairName: props.keyPairName,
+      keyPairPublicKeypath: props.keyPairPublicKeypath,
+      ec2InstanceUsername: props.ec2InstanceUsername,
+    });
+  }
+}
