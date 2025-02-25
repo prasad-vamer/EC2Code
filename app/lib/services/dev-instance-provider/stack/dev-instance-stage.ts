3322e9e940b6d515b750ff83fe458db63e016a6a
prasad-vamer <prasadkrishnakptp@gmail.com>
2025-02-26 01:07:03 +0900

    feat: add DevInstanceStage for logically grouping the stacks

---
+++ app/lib/services/dev-instance-provider/stack/dev-instance-stage.ts
@@ -0,0 +1,34 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { DevInstancServiceProps } from "../../../config/parameters";
+import { DevInstanceNetworkStack } from "./dev-instance-network-stack";
+import { DevInstanceEc2Stack } from "./dev-instance-ec2-stack";
+
+interface DevInstanceStageProps
+  extends cdk.StageProps,
+    DevInstancServiceProps {}
+
+export class DevInstanceStage extends cdk.Stage {
+  constructor(scope: Construct, id: string, props: DevInstanceStageProps) {
+    super(scope, id, props);
+
+    const networking = new DevInstanceNetworkStack(
+      this,
+      "DevInstanceNetworkStack",
+      {
+        // Added env here because If envParameters.env contains a different AWS account or region than DevInstanceNetworkStack,
+        // CDK will not allow cross-stack references.
+        env: props.env,
+      }
+    );
+
+    new DevInstanceEc2Stack(this, "DevInstanceEC2Stack", {
+      env: props.env,
+      vpc: networking.vpc,
+      securityGroup: networking.securityGroup,
+      keyPairName: props.ec2KeyPairName,
+      keyPairPublicKeypath: props.ec2KeyPairPublicKeypath,
+      ec2InstanceUsername: props.ec2InstanceUsername,
+    });
+  }
+}
