0b4e65a5d2adb7b6b1e5dcddeb84972940764198
prasad-vamer <prasadkrishnakptp@gmail.com>
2025-03-04 23:59:08 +0900

    feat: add EC2 instance type configuration for test and dev environments

--- app/lib/config/parameters.ts
+++ app/lib/config/parameters.ts
@@ -1,8 +1,11 @@
 import { Environment } from "aws-cdk-lib";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
 export interface DevInstancServiceProps {
   ec2InstanceUsername?: string;
   ec2KeyPairName: string;
   ec2KeyPairPublicKeypath?: string;
+  ec2InstanceType: ec2.InstanceType;
 }
 export interface EnvironmentConfig {
   env: Environment;
@@ -10,6 +13,23 @@ export interface EnvironmentConfig {
 }
 
 const AppParameters: Record<string, EnvironmentConfig> = {
+  test: {
+    // Stack environment (account and region) for the CDK app
+    env: {
+      account: process.env.AWS_ACCOUNT_ID,
+      region: process.env.AWS_DEFAULT_REGION,
+    },
+    // Service based separation for parameters
+    devInstancServiceProps: {
+      ec2InstanceUsername: "testInstanceAdmin",
+      ec2KeyPairName: "testInstnaceAdmin",
+      ec2KeyPairPublicKeypath: "../tmp/MyEc2Key.pem.pub",
+      ec2InstanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T4G,
+        ec2.InstanceSize.NANO
+      ),
+    },
+  },
   dev: {
     // Stack environment (account and region) for the CDK app
     env: {
@@ -20,7 +40,11 @@ const AppParameters: Record<string, EnvironmentConfig> = {
     devInstancServiceProps: {
       ec2InstanceUsername: "devInstanceAdmin",
       ec2KeyPairName: "devInstnaceAdmin",
-      // ec2KeyPairPublicKeypath: "../tmp/MyEc2Key.pem.pub",
+      ec2KeyPairPublicKeypath: "../tmp/MyEc2Key.pem.pub",
+      ec2InstanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.R8G,
+        ec2.InstanceSize.MEDIUM
+      ),
     },
   },
 };
